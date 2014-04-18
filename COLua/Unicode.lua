ddf0eb07074d87c278b026d5b7a3f77ebe24216c
Wobbo <rene.mellema@gmail.com>
2014-04-18 23:30:23 +0200

    Add Unicode Class for COLua

---
+++ COLua/Unicode.lua
@@ -0,0 +1,26 @@
+local COLua = require "COLua"
+local String = require "COLua.String"
+local unicode = require "unicode"
+
+COLua.Unicode = COLua{"Unicode", String;
+  _char = function(self, code, ...)
+    return COLua.Unicode(unicode.char(code, ...))
+  end,
+  len = function(self)
+    return unicode.len(self.str)
+  end,
+  lower = function(self)
+    return COLua.Unicode(unicode.lower(self.str))
+  end,
+  reverse = function(self)
+    return COLua.Unicode(unicode.reverse(self.str))
+  end,
+  sub = function(self, i, j)
+    return COLua.Unicode(unicode.sub(self.str, i, j))
+  end,
+  upper = function(self)
+    return COLua.Unicode(unicode.upper(self))
+  end}
+
+return COLua.Unicode
+
